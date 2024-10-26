8c209227f97a6001cc683a66d5e4ecfd437c8043
Hesam <hesamdanaey@outlook.com>
2024-10-26 19:01:14 +0330

    feat(public/fonts): Add Raleway font to public and layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,25 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import Header from "@/layouts/Header";
+import { Raleway } from "next/font/google";
+
+import "../styles/globals.css";
+import Header from "@/layouts/Header/Header";
 import Footer from "@/layouts/Footer";
 import { Providers } from "./Providers";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+const raleway = Raleway({
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
@@ -28,9 +34,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${raleway.className} antialiased`}>
         <Providers>
           <Header />
           {children}
